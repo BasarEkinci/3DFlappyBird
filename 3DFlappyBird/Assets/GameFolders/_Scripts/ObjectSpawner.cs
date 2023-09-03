338936ab69b2887b07ae807fd36d6bb11918d350
BasarEkinci <basarekinci02@outlook.com>
2023-09-04 01:20:41 +0300

    Pipe Movement Added

--- 3DFlappyBird/Assets/GameFolders/_Scripts/ObjectSpawner.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ObjectSpawner : MonoBehaviour
-{
-    [SerializeField] GameObject pipe;
-    [SerializeField] List<GameObject> environmentObjects;
-}
