1a5a383bae104fb1a2f5b6981f8c8243e29becd2
BasarEkinci <basarekinci02@outlook.com>
2023-10-04 19:09:33 +0300

    Player Physics Controller Edited

--- 3DFlappyBird/Assets/Scripts/Runtime/Controllers/Player/PlayerPhysicsController.cs
+++ 3DFlappyBird/Assets/Scripts/Runtime/Controllers/Player/PlayerPhysicsController.cs
@@ -1,7 +1,25 @@
-﻿namespace Runtime.Controllers.Player
+﻿using Runtime.Signals;
+using UnityEngine;
+
+namespace Runtime.Controllers.Player
 {
-    public class PlayerPhysicsController
+    public class PlayerPhysicsController : MonoBehaviour
     {
-        
+        private string pointAreaTag = "PointArea";
+        private string pipeTag = "Pipe";
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.CompareTag(pointAreaTag))
+            {
+                CoreGameSignals.Instance.OnPipePassed?.Invoke();
+                return;
+            }
+            if (other.gameObject.CompareTag(pipeTag))
+            {
+                CoreGameSignals.Instance.OnCrashPipe?.Invoke();
+                CoreGameSignals.Instance.OnGameOver?.Invoke();  
+                return;
+            }
+        }
     }
 }
