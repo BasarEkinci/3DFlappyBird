7f94c6db5806eee282e1a9fe8dafcdc2f7f52f77
BasarEkinci <basarekinci02@outlook.com>
2023-10-04 18:42:35 +0300

    Player Manager Added

---
+++ 3DFlappyBird/Assets/Scripts/Runtime/Managers/PlayerManager.cs
@@ -0,0 +1,19 @@
+﻿using Runtime.Data.UnityObjects;
+using Runtime.Data.ValueObjects;
+using UnityEngine;
+
+namespace Runtime.Managers
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        private PlayerData data;
+        private void Awake()
+        {
+            data = GetPlayerData();
+        }
+        private PlayerData GetPlayerData()
+        {
+            return Resources.Load<CD_Player>("Data/CD_Player").PlayerData;
+        }
+    }
+}
